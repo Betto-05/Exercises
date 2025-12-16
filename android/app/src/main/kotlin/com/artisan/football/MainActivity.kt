cc6901a61c1ec5b176b2d965e0c9121d149b6a09
betto-05 <betoelferjani@gmail.com>
2025-12-16 13:44:44 +0200

    initilaizing firebase ( for android only )

--- android/app/src/main/kotlin/com/artisan/football/MainActivity.kt
+++ android/app/src/main/kotlin/com/artisan/football/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.football
+package com.artisan.football
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  // 2. THIS LINE IS REQUIRED
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 3. INITIALIZE FIREBASE
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
+  // This widget is the root of your application.  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
